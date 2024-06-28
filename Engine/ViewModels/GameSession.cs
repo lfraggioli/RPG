0a51f5cdca2b190c1ce092d508508e071395a1df
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2024-06-28 16:28:26 -0300

    first commit

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Player 1";
+            CurrentPlayer.Gold = 1000;
+
+        }
+
+    }
+}
